869569692c0a53bd8e4f11678b385b6d5a5bb64b
Robin <robin.kehl@singular-it.de>
2025-04-07 08:45:48 +0000

    chore: add custom plugin

--- src/example.test.js
+++ src/example.test.js
@@ -2,7 +2,7 @@ import App from './App.vue'
 import {mount} from '@vue/test-utils'
 import ui from '@nuxt/ui/vue-plugin'
 
-it("should rende button", ()=>{
+it("should render button", ()=>{
     const wrapper = mount(App, {
         global: {
             plugins: [ui]
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from "vite"
+import { defineConfig, Plugin } from "vite"
 import vue from "@vitejs/plugin-vue"
-
 import ui from "@nuxt/ui/vite"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     globals: true,
   },
+  optimizeDeps: {
+    exclude: ["@nuxt/ui"]
+  },
   plugins: [
+    {
+      enforce: "pre",
+      name: "test",
+      resolveId: (id)=>{
+        console.log(id)
+        if(id === '#imports') {
+          console.log("resolved")
+        }
+      }
+    } satisfies Plugin,
     vue(),
     ui({
       ui: {
@@ -17,6 +30,6 @@ export default defineConfig({
           neutral: "slate"
         }
       }
-    })
+    }),
   ]
 })
